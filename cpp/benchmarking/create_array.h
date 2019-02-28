c4a891938c5872502867aedaf506054886dc8310
Steven King <stking74@gmail.com>
2019-02-28 02:00:49 +0000

    restructure directory

---
+++ cpp/benchmarking/create_array.h
@@ -0,0 +1,13 @@
+int main(){
+
+    const int array_length = 10;
+    const int array_init = 0;
+
+    int n[array_length];
+    for ( int i = 0 ; i < array_length ; i++ ) {
+
+        n[i] = array_init;
+
+    }
+    return 0;
+}
